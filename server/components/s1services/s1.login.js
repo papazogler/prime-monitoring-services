0cec98c15f0272fee0ba3063f74782bc17741d95
papazogler <papazogler@gmail.com>
2015-02-08 18:56:17 +0200

    Fixed error in login for s1

--- server/components/s1services/s1.login.js
+++ server/components/s1services/s1.login.js
@@ -5,57 +5,56 @@
 var config = require('../../config/environment');
 var http = require("../http-utils/http.request");
 
-function isAuthenticated() {
-  var login = {
-    service: "login",
-    username: config.s1User.username,
-    password: config.s1User.password,
-    appId: config.s1User.appId
-  };
-
-  http.requestWithEncoding(config.s1HttpOptions(login, login, function (err, data) {
-    if (err) {
-      console.send(err);
+(function (){
+  var clientId;
+  function authenticate(callback){
+    if(undefined !== clientId) {
+      callback(null, clientId);
+    }else{
+      var login = {
+        service: "login",
+        username: config.s1User.username,
+        password: config.s1User.password,
+        appId: config.s1User.appId
+      };
+      var q = JSON.stringify(login);
+      http.requestWithEncoding(config.s1HttpOptions(q), q, function (err, data) {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          var loginRes = JSON.parse(data);
+          if (!loginRes.success) {
+            callback(loginRes.error);
+          } else {
+            var auth = {
+              service: "authenticate",
+              clientID: loginRes.clientID,
+              COMPANY: loginRes.objs[0].COMPANY,
+              BRANCH: loginRes.objs[0].BRANCH,
+              MODULE: loginRes.objs[0].MODULE,
+              REFID: loginRes.objs[0].REFID
+            };
+            var q = JSON.stringify(auth);
+            http.requestWithEncoding(config.s1HttpOptions(q), q, function (err, data) {
+              if (err) {
+                callback(err);
+              } else {
+                var authRes = JSON.parse(data);
+                if (!authRes.success) {
+                  callback(authRes.error);
+                } else {
+                  clientId = authRes.clientID;
+                  callback(null, clientId);
+                }
+              }
+            });
+          }
+        }
+      });
     }
-    else {
-      var parse = JSON.parse(data);
-      var vessels = parse.rows;
-      res.json(vessels);
-
-      //console.log(win1253.decode(data));
-    }
-  }));
-
-  var res = {
-    "success": true,
-    "clientID": "Wj8T3tvs...  ...tlrT8",
-    "objs": [
-      {
-        "COMPANY": "1000",
-        "COMPANYNAME": "Demo Company SA",
-        "BRANCH": "1000",
-        "BRANCHNAME": "Athens",
-        "MODULE": "0",
-        "MODULENAME": "Center",
-        "REFID": "1",
-        "REFIDNAME": "Administrator"
-      }
-    ]
   };
 
-  var auth = {
-    service: " authenticate",
-    clientID: "Wj8T3tvs...  ...tlrT8",
-    COMPANY: "1000",
-    BRANCH: "1000",
-    MODULE: "0",
-    REFID: "1"
-  };
-}
+  module.exports.authenticate= authenticate;
+})();
 
-function getClientID() {
-  return "9J8pJsP8KKnwH68bDKObDKL5TLHhMND9UKHoKoKrGs56MIKqC6X2JKbvPrObDKL9KKzdR5DMH6bbLN1LTL0bDKLHGKztR58bDK9vU4HAKIKrGt4bD31ZT7PO9JL59JL4HL4bDqPnLLHmPKLL9JL5JLLMLcLaJa9mOLTJQ7DD9JOmI2KqC5T5GLHLINLXJKDLOa55H64bDKDF9JGmKKn6M50bD31G9JGmK4D6M4D1I5b2Gs9GGqabDKKbDZ1JKaLvHKmbDqD8SIKrHb0bDKDoLarPTrKbDKLXK4XdQ4DAR4nrLNbDOqrZQ71AML55ML4bDqH4";
-};
-
-module.exports.isAuthenticated = isAuthenticated;
-module.exports.getClientID = getClientID;
--- server/components/s1services/s1.query.js
+++ server/components/s1services/s1.query.js
@@ -7,46 +7,58 @@ var http = require("../http-utils/http.request");
 var s1 = require("./s1.login");
 
 function execute(query, callback) {
-  var q = {
-    "service": "getSqlData",
-    "clientID": s1.getClientID(),
-    "appId": "156",
-    "sql": query
-  };
-  q = JSON.stringify(q);
-  http.requestWithEncoding(config.s1HttpOptions(q), q, function (err, data) {
-    if (err) {
+
+  s1.authenticate(function(err, clientId) {
+    if(err) {
       console.log(err);
-    }
-    else {
-      var parse = JSON.parse(data);
-      callback(parse.rows);
+    }else {
+      var q = {
+        "service": "getSqlData",
+        "clientID": clientId,
+        "appId": config.s1User.appId,
+        "sql": query
+      };
+      q = JSON.stringify(q);
+      http.requestWithEncoding(config.s1HttpOptions(q), q, function (err, data) {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          var parse = JSON.parse(data);
+          callback(parse.rows);
+        }
+      });
     }
   });
 }
 
 function executeWithPaging(query, orderBy, page, maxcount, callback) {
-  var skipRows = (page -1) * maxcount;
-  var pagination = ' order by ' + orderBy + ' offset ' + skipRows + ' rows fetch next ' + maxcount + ' rows only';
-
-  var q = {
-    "service": "getSqlData",
-    "clientID": s1.getClientID(),
-    "appId": "156",
-    "sql": query + pagination
-  };
-  q = JSON.stringify(q);
-  http.requestWithEncoding(config.s1HttpOptions(q), q, function (err, data) {
+  s1.authenticate(function (err, clientId) {
     if (err) {
-      console.send(err);
-    }
-    else {
-      var parse = JSON.parse(data);
-      callback(parse.rows);
+      console.log(err);
+    } else {
+      var skipRows = (page - 1) * maxcount;
+      var pagination = ' order by ' + orderBy + ' offset ' + skipRows + ' rows fetch next ' + maxcount + ' rows only';
+
+      var q = {
+        "service": "getSqlData",
+        "clientID": clientId,
+        "appId": config.s1User.appId,
+        "sql": query + pagination
+      };
+      q = JSON.stringify(q);
+      http.requestWithEncoding(config.s1HttpOptions(q), q, function (err, data) {
+        if (err) {
+          console.send(err);
+        }
+        else {
+          var parse = JSON.parse(data);
+          callback(parse.rows);
+        }
+      });
     }
   });
 }
-
 
 module.exports.execute = execute;
 module.exports.executeWithPaging = executeWithPaging;
