25699ce55483abbef0448986c50390fafed391fe
papazogler <papazogler@gmail.com>
2015-02-09 20:39:00 +0200

    Fixed instruments count by ship query

--- server/api/ship/ship.controller.js
+++ server/api/ship/ship.controller.js
@@ -7,7 +7,7 @@ exports.index = function (req, res) {
   var page = req.query.page || 1;
   var count = req.query.count || 10;
 
-  var q = "select s.name Name, count( * ) InstrumentsCount, sum( case v.status when 3 then 1 else 0 end ) as Operative, sum( case v.status when 1 then 1 when 2 then 1 else 0 end ) as Defective, sum( case v.status when 5 then 1 else 0 end ) InService, sum( case when datediff(m, getdate(), v.date01) < 1 then 1 else 0 end ) CertificateRenewal from( select d.sncode, max(f.trndate) trndate, max(f.date01) date01, f.CCCSTATUS as Status, f.CCCCUSTSHIP, f.trdr from mtrdoc d inner join FINDOC f on d.findoc = f.findoc where f.cccstatus in (1, 2, 3, 5) group by d.sncode, f.CCCSTATUS, f.CCCCUSTSHIP, f.trdr ) v inner join CCCCUSTSHIP s on v.CCCCUSTSHIP = s.CCCCUSTSHIP and v.trdr = s.trdr where s.trdr = " + req.user.s1data.id  + " group by s.name order by s.name ";
+  var q = 'select s.name Name, count( * ) InstrumentsCount, sum( case f.CCCSTATUS when 3 then 1 else 0 end ) as Operative, sum( case f.CCCSTATUS when 1 then 1 when 2 then 1 else 0 end ) as Defective, sum( case f.CCCSTATUS when 5 then 1 else 0 end ) InService, sum( case when datediff(m, getdate(), f.date01) < 1 then 1 else 0 end ) CertificateRenewal from (select d.sncode, max(f1.findoc) findoc from mtrdoc d inner join FINDOC f1 on d.findoc = f1.findoc where d.sncode is not null group by d.sncode) v inner join FINDOC f on f.findoc = v.findoc inner join CCCCUSTSHIP s on f.CCCCUSTSHIP = s.CCCCUSTSHIP and f.trdr = s.trdr where s.trdr =' + req.user.s1data.id  + ' group by s.name order by s.name';
 
   s1.execute(q, function (vessels) {
     return res.json(vessels);
@@ -20,3 +20,4 @@ exports.show = function (req, res) {
 function handleError(res, err) {
   return res.send(500, err);
 }
+
