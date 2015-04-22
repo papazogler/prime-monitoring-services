97522cf1c5241fa404ca782d619ee664d45282ac
papazogler <papazogler@gmail.com>
2015-04-22 21:47:21 +0300

    Different home page for admin

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -1,26 +1,32 @@
 'use strict';
 
 angular.module('primeMonitoringServicesApp')
-  .controller('LoginCtrl', function ($scope, Auth, $location) {
-    $scope.user = {};
-    $scope.errors = {};
+	.controller('LoginCtrl', function ($scope, Auth, $location) {
+		$scope.user = {};
+		$scope.errors = {};
 
-    $scope.login = function(form) {
-      $scope.submitted = true;
+		$scope.login = function (form) {
+			$scope.submitted = true;
 
-      if(form.$valid) {
-        Auth.login({
-          username: $scope.user.username,
-          password: $scope.user.password
-        })
-        .then( function() {
-          // Logged in, redirect to home
-          $location.path('/');
-        })
-        .catch( function(err) {
-          $scope.errors.other = err.message;
-        });
-      }
-    };
+			if (form.$valid) {
+				Auth.login({
+					username: $scope.user.username,
+					password: $scope.user.password
+				})
+					.then(function () {
+						// Logged in, redirect to home
+						Auth.isAdminAsync(function (isAdmin) {
+							if (isAdmin) {
+								$location.path('/admin')
+							} else {
+								$location.path('/ship');
+							}
+						});
+					})
+					.catch(function (err) {
+						$scope.errors.other = err.message;
+					});
+			}
+		};
 
-  });
+	});
